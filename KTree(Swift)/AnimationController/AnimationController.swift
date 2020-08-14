e2530b32af5ac4b864ca60ac55a94d3ef85614eb
Kyle Burns <hensonmartha1@gmail.com>
2020-08-14 11:52:03 -0500

    Initial Commit

---
+++ KTree(Swift)/AnimationController/AnimationController.swift
@@ -0,0 +1,30 @@
+//
+//  AnimationController.swift
+//  KTree(Swift)
+//
+//  Created by Kyle Burns on 8/14/20.
+//  Copyright © 2020 Kyle Burns. All rights reserved.
+//
+
+import UIKit
+
+class AnimationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ KTree(Swift)/TabController/TabController.swift
@@ -0,0 +1,20 @@
+//
+//  TabController.swift
+//  KTree(Swift)
+//
+//  Created by Kyle Burns on 8/14/20.
+//  Copyright © 2020 Kyle Burns. All rights reserved.
+//
+
+import UIKit
+
+class TabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewControllers = [AnimationController()]
+        
+    }
+
+}
